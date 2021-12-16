4b147deef9c3e68b11103db1b89f8d6b1bc0d6fe
Gregory Delgado <gregd92694@yahoo.com>
2021-12-16 14:52:44 -0600

    Home page created and styling added.

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import './HomePage.css'
 
 function HomePage(props){
     return(
-        <div>
-            <h1>HomePage</h1>
+        <div className="background-container">
+            <div className="title-holder">
+            <h1>AFTER SERVICE</h1>
+            <h3>Service Member to Civilian Transitional Guidance Group</h3>  
+            </div>
+
+            <div className='intro-paragraph'>
+            <h4>Site tailored by former service members, for service members
+            making the transition from the military and gradually 
+            into civilian life.
+            </h4>
+            </div>
+
+            <div className='route-buttons'>
+            <button  href="/login">Login</button>
+            <button  className="middle-button" href="/signup">Sign-up</button>
+            <button  href="/about">About</button>
+            </div>
         </div>
     )
 }
